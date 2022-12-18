78f4aa2f3a3060e2c5d92e473b0f5578f7c76151
Tany546466 <106480090+Tany546466@users.noreply.github.com>
2022-12-18 16:01:14 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+pygame.init()
+
+width = 500
+height = 500
+
+win = pygame.display.set_mode((width, height))
+
+white = (255, 255, 255)
+
+FPS = 60
+clock = pygame.time.Clock()
+
+def load_img(name):
+    img = pygame.image.load(name)
+    img = img.convert()
+    colorkey = img.get_at((0, 0))
+    img.set_colorkey(colorkey)
+    return img
+
+class Inginirium(pygame.sprite.Sprite):
+
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.image = load_img('')
+        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(width)
+        self.rect.y = random.randrange(height)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+class PlayerObject:
+    def __init__(self):
+        self.health = 5
+        self.width = 0
+        self.height = 0
+        self.image = pygame.image.load('*путь до файла*')
+        self.rect = self.image.get_rect()
+    def move_up(self):
+        pass
+    def move_down(self):
+        pass
+    def move_left(self):
+        pass
+    def move_right(self):
+        pass
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    win.fill(white)
+    pygame.display.update()
+    clock.tick(FPS)
+
